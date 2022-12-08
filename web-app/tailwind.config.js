13cd42561dd4d94f68974dd5619146b95523f6f6
Mark Ibanez <mark.ibanez@gmail.com>
2022-12-08 21:56:21 +0800

    Tailwind + Tailwind Elements

---
+++ web-app/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['"Space Grotesk"', 'sans-serif'],
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"Space Grotesk"', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
+}
