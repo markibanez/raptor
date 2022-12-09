651c1c08c89a68c1c78f5b3a91b03aa8c440298b
Mark Ibanez <mark.ibanez@gmail.com>
2022-12-09 10:08:08 +0800

    move to daisy ui

--- web-app/tailwind.config.js
+++ web-app/tailwind.config.js
@@ -1,21 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    fontFamily: {
-      sans: ['"Space Grotesk"', 'sans-serif'],
+    content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+            sans: ['"Space Grotesk"', 'sans-serif'],
+        },
+        extend: {
+            fontFamily: {
+                sans: ['"Space Grotesk"', 'sans-serif'],
+            },
+        },
     },
-    extend: {
-      fontFamily: {
-        sans: ['"Space Grotesk"', 'sans-serif'],
-      }
-    },
-  },
-  plugins: [
-    require('tw-elements/dist/plugin')
-  ],
-}
+    plugins: [require('daisyui')],
+};
